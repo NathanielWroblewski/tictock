7652f672e3160416620398e8e4b9f119d76a6747
Nathaniel Wroblewski <nathanielwroblewski@gmail.com>
2013-09-26 22:42:00 -0700

    controller makes api calls to get time

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -3,4 +3,12 @@ class PlacesController < ApplicationController
   def index
   end
 
+  def create
+    Geocoder.configure(timeout: 300)
+    latlong = Geocoder.coordinates(params[:place])
+    timezone = Timezone::Zone.new(latlon: latlong)
+
+    render json: { time: timezone.time(Time.now) }
+  end
+
 end
